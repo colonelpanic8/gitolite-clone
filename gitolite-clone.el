b4e4f419683e43230eb06d5308a192746bb30e4c
Ivan Malison <imalison@uber.com>
2015-08-15 20:03:29 -0700

    Add docstring for gitoline-clone entry function and change its name to gitolite-clone

--- gitolite-clone.el
+++ gitolite-clone.el
@@ -75,7 +75,22 @@
   (completing-read "Choose a repository:" (gitolite-clone-get-projects username host)))
 
 ;;;###autoload
-(defun gitolite-clone-clone-repo (&optional username host determine-target action)
+(defun gitolite-clone (&optional username host determine-target action)
+  "Clone a gitolite repo to be selected by `completing-read'.
+
+USERNAME and HOST will be used to determine how to talk to
+gitolite using ssh.  They default to `gitolite-clone-username' and
+`gitolite-clone-host' respectively.
+
+DETERMINE-TARGET is a function with signature identical to that
+of `gitolite-clone-default-determine-target'.  It will determine
+the path to which the repository should be cloned, and it
+defaults to `gitolite-clone-default-determine-target'.
+
+ACTION is a function that will be executed once the repository
+has been cloned.  It's signature should be that of
+`gitolite-clone-default-action', which is also the default value
+for this argument."
   (interactive)
   (unless username (setq username gitolite-clone-username))
   (unless host (setq host gitolite-clone-host))
