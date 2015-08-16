31b3998a7fd2a32c92236f0029fe236eb169b615
Ivan Malison <imalison@uber.com>
2015-08-15 20:02:40 -0700

    Require emacs24 because lexical binding is used

--- gitolite-clone.el
+++ gitolite-clone.el
@@ -6,7 +6,7 @@
 ;; Keywords: gitolite clone git
 ;; URL: https://github.com/IvanMalison/gitolite-cloone
 ;; Version: 0.1.0
-;; Package-Requires: ((dash "2.10.0") (s "1.9.0") (pcache "0.3.1"))
+;; Package-Requires: ((dash "2.10.0") (s "1.9.0") (pcache "0.3.1") (emacs "24"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
